2d3facc7e000c892a38acd5747460d2985c30207
PokeHooK <71217920+pokepollo@users.noreply.github.com>
2023-03-26 05:55:38 +0200

    Actualizacion a V2
    Aqui e agregado la funcion de enviar una foto predeterminada cada vez que se una un usuario.

--- welcome.py
+++ welcome.py
@@ -6,13 +6,19 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_member_join(member):
+    # Define el canal donde quieres enviar el mensaje de bienvenida
+    canal = client.get_channel(ID DE CANAL)
 
-    # Id del canal donde quieres enviar el mensaje de bienvenida
-    canal = client.get_channel(ID DEL CANAL)
+    # Carga la imagen que deseas enviar
+    with open('FOTO.png', 'rb') as f:
+        imagen = discord.File(f)
 
-    # Mensaje de bienvenida
-    mensaje_bienvenida = f'Bienvenido {member.mention} al servidor de Discord!'
+    # Crea el mensaje de bienvenida con la imagen adjunta
+    mensaje_bienvenida = f'¡Bienvenido {member.mention} al servidor de Discord! :tada: :confetti_ball:\n\n'
+    mensaje_bienvenida += 'Esperamos que te sientas como en casa, no dudes en presentarte en el canal correspondiente y comenzar a conocer gente nueva.\n\n'
+    mensaje_bienvenida += 'Recuerda revisar las normas del servidor antes de comenzar a participar.\n\n'
+    mensaje_bienvenida += '¡Que tengas un excelente día!'
 
-    # Envía el mensaje al canal de bienvenida
-    await canal.send(mensaje_bienvenida)
+    await canal.send(mensaje_bienvenida, file=imagen)
+
 client.run('TOKEN')
