2b2bc9619c1b96ccb6bce11c4823cf1942d98139
PokeHooK <71217920+pokepollo@users.noreply.github.com>
2023-03-27 00:25:27 +0200

    Update WelcomeV2.py
    Agregado mensaje nuevo en la consola, Mensaje al salir del servidor y Añadir un Rol cuando alguien nuevo se une al servidor + Mensajes en Embed.

--- WelcomeV2.py
+++ WelcomeV2.py
@@ -1,8 +1,23 @@
 import discord
+import asyncio
+import datetime
+import colorama
+from colorama import Fore, Style
 
 intents = discord.Intents.default()
 intents.members = True 
 client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+
+    print(Fore.GREEN + '==============================================')
+    print('¡El bot de bienvenida está en línea!')
+    print('Conectado como:', Fore.YELLOW + client.user.name, '#' + client.user.discriminator)
+    print('ID del bot:', Fore.YELLOW + str(client.user.id))
+    print('Hora de inicio del BOT:', Fore.CYAN + datetime.datetime.now().strftime('%H:%M:%S'))
+    print(Fore.GREEN + '==============================================')
+    print(Style.RESET_ALL)
 
 @client.event
 async def on_member_join(member):
@@ -10,15 +25,34 @@ async def on_member_join(member):
     canal = client.get_channel(ID DE CANAL)
 
     # Carga la imagen que deseas enviar
-    with open('FOTO.png', 'rb') as f:
+    with open('IMAGE.png', 'rb') as f:
         imagen = discord.File(f)
 
-    # Crea el mensaje de bienvenida con la imagen adjunta
-    mensaje_bienvenida = f'¡Bienvenido {member.mention} al servidor de Discord! :tada: :confetti_ball:\n\n'
-    mensaje_bienvenida += 'Esperamos que te sientas como en casa, no dudes en presentarte en el canal correspondiente y comenzar a conocer gente nueva.\n\n'
-    mensaje_bienvenida += 'Recuerda revisar las normas del servidor antes de comenzar a participar.\n\n'
-    mensaje_bienvenida += '¡Que tengas un excelente día!'
+    # Define el mensaje de bienvenida en formato embed
+    embed = discord.Embed(title=f'¡Bienvenido {member.name}!', description='Esperamos que te sientas como en casa.', color=0x00ff00)
+    embed.set_thumbnail(url=member.avatar.url)
+    embed.add_field(name='Normas del servidor', value='Por favor, asegúrate de leer las normas del servidor antes de comenzar a participar.')
+    embed.set_footer(text='¡Que tengas un excelente día!')
 
-    await canal.send(mensaje_bienvenida, file=imagen)
+    # Asignar un rango al nuevo miembro
+    role = discord.utils.get(member.guild.roles, name="NOMBRE DE ROL")
+    await member.add_roles(role)
 
-client.run('TOKEN')
+    await canal.send(file=imagen, embed=embed)
+
+@client.event
+async def on_member_remove(member):
+    # Define el canal donde quieres enviar el mensaje de despedida
+    canal = client.get_channel(ID DE CANAL)
+
+    # Carga la imagen que deseas enviar
+    with open('IMAGE.png', 'rb') as f:
+        imagen = discord.File(f)
+
+    # Define el mensaje de despedida en formato embed
+    embed = discord.Embed(title=f'¡{member.name} ha abandonado el servidor!', description='¡Esperamos verte pronto de nuevo!', color=0xff0000)
+    embed.set_thumbnail(url=member.avatar.url)
+
+    await canal.send(file=imagen, embed=embed)
+
+client.run('BOT TOKEN')
